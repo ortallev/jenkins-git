66e7d16d9886d8806463f4d24aa7f63a5606c518
ortal azulay levi <44336251+ortallev@users.noreply.github.com>
2019-04-02 19:34:24 +0300

    commitkosemek

--- script.java
+++ script.java
@@ -4,7 +4,7 @@ public class script {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("world is a bitch");
+		System.out.println("hello world is a bitch");
 
 	}
 
